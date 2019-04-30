4e1467d0bc0de66201fa6811fc07470663462284
Pankaj Pramanik <pankaj.me2k9@gmail.com>
2019-04-30 23:22:44 +0600

    Update sheetsAPI.js

--- client/app/utilities/sheetsAPI.js
+++ client/app/utilities/sheetsAPI.js
@@ -1,7 +1,7 @@
 export const credentials = {
-    apiKey: 'AIzaSyD2QLd-chggknQQSffwy0gyVdWILK2OH7U',
+    apiKey: 'Your API key in here',
     discoveryDocs: ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
-    spreadsheetId: "1vXwIz89L7N7-R2EnqbcaFjFHE1W0HvS1vwv3oBGMzKk",
+    spreadsheetId: "Your spread sheetId in here",
     ranges: [
         "California",
         "Northwest",
